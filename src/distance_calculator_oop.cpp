a0e49c886a1514b27ca8af1173ba9e13a7568bcd
jay <jhjo1997@naver.com>
2022-09-17 18:01:23 +0900

    Debug array size check

--- src/distance_calculator_oop.cpp
+++ src/distance_calculator_oop.cpp
@@ -22,26 +22,26 @@
 //   goal.pose.orientation.z = goal_msg.pose.orientation.z;
 //   goal.pose.orientation.w = goal_msg.pose.orientation.w;
 // }
-float total_vel = 0.0;
-int status_flag;
-int first_time; 
-uint8_t debug_string;
-geometry_msgs::Pose current_robot_pose;
-geometry_msgs::Pose prev_current_robot_pose;
-double move_base_GlobalPlanner_plan_Time;
-float first_global_path_distance;
-float global_path_percentage;
-float remaining_time;
-double msg_global_path_distance;
-double traveled_distance;
-float average_total_vel;
-double remaining_percentage;
-bool global_path_flag = false;
-
 
 class Distance_TimeCalculator
 {
   public:
+    float total_vel = 0.0;
+    int status_flag=0;
+    int first_time=1; 
+    uint8_t status_info_=0;
+    geometry_msgs::Pose current_robot_pose;
+    geometry_msgs::Pose prev_current_robot_pose;
+    double move_base_GlobalPlanner_plan_Time=0.0;
+    float first_global_path_distance=0.0;
+    float global_path_percentage=0.0;
+    float remaining_time=0.0;
+    double msg_global_path_distance=0.0;
+    double traveled_distance=0.0;
+    float average_total_vel=0.0;
+    double remaining_percentage=0.0;
+    bool global_path_flag = false;
+
     Distance_TimeCalculator(ros::NodeHandle *n)
     {
       feedback_sub = n->subscribe("move_base/feedback", 1000, &Distance_TimeCalculator::get_feedback_cb, this);
@@ -52,7 +52,15 @@ class Distance_TimeCalculator
 
 void get_status_cb(const actionlib_msgs::GoalStatusArray &status_msg) {
   // status_flag = status_msg.status_list[0].status;
-  debug_string = status_msg.status_list[0].status;
+  int is=0;
+  if (!status_msg.status_list.empty()) {
+    for(size_t i=0; i<status_msg.status_list.size()-1; i++)
+    {
+      is++;
+    }
+      status_info_ = status_msg.status_list[is].status;
+  }
+  else ROS_INFO("status_list array is empty");
   ROS_INFO("status flag: %u", status_flag);
 }
 
@@ -84,20 +92,21 @@ void get_globalpath_cb(const nav_msgs::Path &globalpath_msgs) {
  float pre_global_path_distance=0.0;
 
   if (status_flag==1){
-
-  for (size_t i=0; i < globalpath_msgs.poses.size()-1; i++)
-  {
-   current_a_x = globalpath_msgs.poses[i].pose.position.x;
-   current_b_x = globalpath_msgs.poses[i+1].pose.position.x;
-   current_a_y = globalpath_msgs.poses[i].pose.position.y;
-   current_b_y = globalpath_msgs.poses[i+1].pose.position.y;
-   global_path_distance += hypot((current_b_x - current_a_x), (current_b_y - current_a_y));
-   msg_global_path_distance = global_path_distance;
-   // ROS_INFO("hypot value : %lf", hypot((current_b_x - current_a_x), (current_b_y - current_a_y)));
-   //global_path_distance += sqrt(pow(current_b_x - current_a_x,2) + pow(current_b_y - current_a_y,2));
-   if(counter_N2==1) first_global_path_distance = global_path_distance;
-   global_path_percentage = ((first_global_path_distance-global_path_distance)/first_global_path_distance)*100;
-  }
+    if ( !globalpath_msgs.poses.empty()) {
+      for (size_t i=0; i < globalpath_msgs.poses.size()-1; i++)
+      {
+       current_a_x = globalpath_msgs.poses[i].pose.position.x;
+       current_b_x = globalpath_msgs.poses[i+1].pose.position.x;
+       current_a_y = globalpath_msgs.poses[i].pose.position.y;
+       current_b_y = globalpath_msgs.poses[i+1].pose.position.y;
+       global_path_distance += hypot((current_b_x - current_a_x), (current_b_y - current_a_y));
+       msg_global_path_distance = global_path_distance;
+       // ROS_INFO("hypot value : %lf", hypot((current_b_x - current_a_x), (current_b_y - current_a_y)));
+       //global_path_distance += sqrt(pow(current_b_x - current_a_x,2) + pow(current_b_y - current_a_y,2));
+       if(counter_N2==1) first_global_path_distance = global_path_distance;
+       global_path_percentage = ((first_global_path_distance-global_path_distance)/first_global_path_distance)*100;
+      }
+    }
     remaining_time = global_path_distance/average_total_vel;
     //ROS_INFO("global_path_percentage : %f ", global_path_percentage);
     pre_global_path_distance = global_path_distance;
@@ -133,7 +142,6 @@ void get_feedback_cb(const move_base_msgs::MoveBaseActionFeedback &feedback_msgs
   remaining_percentage = traveled_distance/(msg_global_path_distance+traveled_distance)*100;
   }
 }
-
   private:
     ros::Subscriber feedback_sub;
     ros::Subscriber status_sub;
@@ -162,15 +170,15 @@ int main(int argc, char **argv)
     //   const ros::master::TopicInfo& info = *it;
     //   std::cout << "topic_" << it - master_topics.begin() << ": " << info.name << std::endl;
     // }
-    if ((ros::Time::now().toSec() - move_base_GlobalPlanner_plan_Time) < 2.0) {status_flag=1; }
-    else { status_flag=0; remaining_time = 0.0; msg_global_path_distance=0.0; first_global_path_distance=0.0; traveled_distance=0.0; first_time=0; remaining_percentage=0.0; global_path_flag=false;}
+    if ((ros::Time::now().toSec() - Dt.move_base_GlobalPlanner_plan_Time) < 2.0) {Dt.status_flag=1; }
+    else { Dt.status_flag=0; Dt.remaining_time = 0.0; Dt.msg_global_path_distance=0.0; Dt.first_global_path_distance=0.0; Dt.traveled_distance=0.0; Dt.first_time=0; Dt.remaining_percentage=0.0; Dt.global_path_flag=false;}
     
 
-    distancetimecalculator_msg.distance_remaining = msg_global_path_distance;
-    distancetimecalculator_msg.arrival_time = remaining_time;
-    distancetimecalculator_msg.distance_robot_traveled = traveled_distance;
-    distancetimecalculator_msg.remaining_distance_percentage = remaining_percentage;
-    distancetimecalculator_msg.status_info = debug_string;
+    distancetimecalculator_msg.distance_remaining = Dt.msg_global_path_distance;
+    distancetimecalculator_msg.arrival_time = Dt.remaining_time;
+    distancetimecalculator_msg.distance_robot_traveled = Dt.traveled_distance;
+    distancetimecalculator_msg.remaining_distance_percentage = Dt.remaining_percentage;
+    distancetimecalculator_msg.status_info = Dt.status_info_;
     distancetimecalculator_pub.publish(distancetimecalculator_msg);
     ros::spinOnce();
 
